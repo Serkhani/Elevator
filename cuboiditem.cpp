8ad830cd2291f4eb2dba5977351b1e2f9784c4b7
Serkhani <deviper03@gmail.com>
2023-04-29 22:53:47 +0000

    added cuboid item

--- cuboiditem.cpp
+++ cuboiditem.cpp
@@ -1,29 +1,9 @@
 #include "cuboiditem.h"
 
-CuboidItem::CuboidItem(qreal width, qreal height, qreal depth, QGraphicsItem *parent = nullptr)
+CuboidItem::CuboidItem(qreal width, qreal height, qreal depth, QGraphicsItem *parent)
         : QGraphicsItem(parent), m_width(width), m_height(height), m_depth(depth)
     {
         setFlag(ItemIsSelectable);
         setFlag(ItemIsMovable);
     }
 
-CuboidItem::QRectF boundingRect() const override
-    {
-        return QRectF(-m_width / 2, -m_height / 2, m_width, m_height);
-    }
-
-CuboidItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr) override
-    {
-        // Draw cuboid using QPainter
-        painter->setPen(Qt::black);
-        painter->setBrush(Qt::NoBrush);
-        painter->drawRect(-m_width / 2, -m_height / 2, m_width, m_height);
-        painter->drawLine(-m_width / 2, -m_height / 2, -m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2);
-        painter->drawLine(m_width / 2, -m_height / 2, m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2);
-        painter->drawLine(m_width / 2, m_height / 2, m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2);
-        painter->drawLine(-m_width / 2, m_height / 2, -m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2);
-        painter->drawLine(-m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2, m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2);
-        painter->drawLine(m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2, m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2);
-        painter->drawLine(m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2, -m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2);
-        painter->drawLine(-m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2, -m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2);
-    }
--- cuboiditem.h
+++ cuboiditem.h
@@ -2,11 +2,34 @@
 #define CUBOIDITEM_H
 
 #include <QGraphicsItem>
-
+#include <QPainter>
 class CuboidItem : public QGraphicsItem
 {
 public:
-    CuboidItem();
+    CuboidItem(qreal width, qreal height, qreal depth, QGraphicsItem *parent = nullptr);
+    QRectF boundingRect()
+    {
+        return QRectF(-m_width / 2, -m_height / 2, m_width, m_height);
+    }
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr)
+    {
+        // Draw cuboid using QPainter
+        painter->setPen(Qt::black);
+        painter->setBrush(Qt::SolidPattern);
+        painter->drawRect(-m_width / 2, -m_height / 2, m_width, m_height);
+        painter->drawLine(-m_width / 2, -m_height / 2, -m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2);
+        painter->drawLine(m_width / 2, -m_height / 2, m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2);
+        painter->drawLine(m_width / 2, m_height / 2, m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2);
+        painter->drawLine(-m_width / 2, m_height / 2, -m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2);
+        painter->drawLine(-m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2, m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2);
+        painter->drawLine(m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2, m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2);
+        painter->drawLine(m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2, -m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2);
+        painter->drawLine(-m_width / 2 - m_depth / 2, m_height / 2 - m_depth / 2, -m_width / 2 - m_depth / 2, -m_height / 2 - m_depth / 2);
+    }
+private:
+    qreal m_width;
+    qreal m_height;
+    qreal m_depth;
 };
 
 #endif // CUBOIDITEM_H
